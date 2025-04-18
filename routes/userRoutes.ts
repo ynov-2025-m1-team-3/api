10da6616cc4b8c2b25bb486473dd9f0d1daf6718
Rémy <remy.berjonneau@gmail.com>
2025-04-18 10:45:29 +0200

    chore eslint

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,8 +1,8 @@
-import * as userController from '../controllers/userController';
-import type { FastifyInstance } from 'fastify';
+import * as userController from "../controllers/userController";
+import type { FastifyInstance } from "fastify";
 
 export default async function userRoutes(fastify: FastifyInstance) {
-  fastify.get('/users', userController.getUsersController);
-  fastify.get('/users/:id', userController.getUserByIdController);
-  fastify.delete('/users/:id', userController.deleteUserController);
+  fastify.get("/users", userController.getUsersController);
+  fastify.get("/users/:id", userController.getUserByIdController);
+  fastify.delete("/users/:id", userController.deleteUserController);
 }
